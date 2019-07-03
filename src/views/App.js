182e5cf6c65459f84daf39a9af8805f80e6b6088
Ali Movahedi <ali_movahedi@aol.com>
2019-07-03 17:54:56 +0430

    fix: fix Dropdown Menu

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
+import 'rc-dropdown/assets/index.css';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import 'Root/styles/base.less';
 
