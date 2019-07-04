7864cf554fcf637554a3e2461bf638f6f3883fe0
Ali Movahedi <ali_movahedi@aol.com>
2019-07-04 20:31:24 +0430

    feat: add myrequests/DISABLE reducer handler

--- src/reducers/myrequests.js
+++ src/reducers/myrequests.js
@@ -13,6 +13,19 @@ export default (state = [], action) => {
       ];
     }
 
+    case types.myrequests.DISABLE: {
+      const index = state.findIndex(i => i.id === action.id);
+
+      return [
+        ...state.slice(0, index),
+        {
+          ...state[index],
+          disable: true,
+        },
+        ...state.slice(index + 1),
+      ];
+    }
+
     default: {
       return state;
     }
