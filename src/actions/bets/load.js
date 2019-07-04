7308662d647c6d4b95e0e80da4d8a63bd3a04158
Ali Movahedi <ali_movahedi@aol.com>
2019-07-04 13:58:12 +0430

    feat: use global.tronWeb instead customized tronweb

--- src/actions/bets/load.js
+++ src/actions/bets/load.js
@@ -1,12 +1,11 @@
 import types from 'Root/actions';
 import store from 'Root/store';
-import tronweb from 'Root/tronweb';
 
 export default (bets) => {
   store.dispatch({
     type: types.bets.LOAD,
     bets: bets.filter(
-      bet => bet.creator !== tronweb.defaultAddress.base58,
+      bet => bet.creator !== global.tronWeb.defaultAddress.base58,
     ),
   });
 };
--- src/actions/myrequests/load.js
+++ src/actions/myrequests/load.js
@@ -1,13 +1,12 @@
 import types from 'Root/actions';
 import store from 'Root/store';
-import tronweb from 'Root/tronweb';
 
 export default (bets) => {
   store.dispatch({
     type: types.myrequests.LOAD,
     bets: bets.filter(
       bet => bets.acceptor === 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb'
-      && bet.creator === tronweb.defaultAddress.base58,
+      && bet.creator === global.tronWeb.defaultAddress.base58,
     ),
   });
 };
