c7813acd96cd8485cb31da1ffcfc071415528dd2
Ali Movahedi <ali_movahedi@aol.com>
2019-07-04 17:18:14 +0430

    fix: fix mybets load action

--- src/actions/bets/load.js
+++ src/actions/bets/load.js
@@ -5,7 +5,10 @@ export default (bets) => {
   store.dispatch({
     type: types.bets.LOAD,
     bets: bets.filter(
-      bet => bet.creator !== global.tronWeb.defaultAddress.base58,
+      bet => bet.creator !== global.tronWeb.defaultAddress.base58
+      && !bets.done
+      && !bets.disable
+      && bets.acceptor === 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb',
     ),
   });
 };
