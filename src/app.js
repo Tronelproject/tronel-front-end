8e9ca402f4c02a5e5d98991068b6361aad1d5be6
Ali Movahedi <ali_movahedi@aol.com>
2019-07-04 14:26:32 +0430

    feat: load bets when app started

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
+import loadBets from 'Root/actions/bets/load';
+import loadMyBets from 'Root/actions/mybets/load';
+import loadMyRequests from 'Root/actions/myrequests/load';
+import getBets from 'Root/helpers/getBets';
 import App from './views';
 
-render(
-  <App />,
-  global.document.getElementById('root'),
-);
+(async () => {
+  render(
+    <App />,
+    global.document.getElementById('root'),
+  );
+
+  if (!global.tronWeb) {
+    // show alert
+    console.error('there is no tronlink');
+  } else {
+    const bets = await getBets();
+
+    loadBets(bets);
+    loadMyBets(bets);
+    loadMyRequests(bets);
+  }
+})();
 
 if (module.hot) {
   module.hot.accept();
