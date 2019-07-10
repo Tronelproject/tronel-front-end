98472605a89a050db480f3eda14f9b6de1643dee
Ali Movahedi <ali_movahedi@aol.com>
2019-07-10 19:35:05 +0430

    fix: fix loading my bets

--- src/actions/mybets/load.js
+++ src/actions/mybets/load.js
@@ -5,8 +5,12 @@ export default (bets) => {
   store.dispatch({
     type: types.mybets.LOAD,
     bets: bets.filter(
-      bet => bet.acceptor !== 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb'
-      && bet.creator === global.tronWeb.defaultAddress.base58,
+      bet => (
+        bet.acceptor !== 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb'
+        && bet.creator === global.tronWeb.defaultAddress.base58
+      ) || (
+        bet.acceptor === global.tronWeb.defaultAddress.base58
+      ),
     ),
   });
 };
